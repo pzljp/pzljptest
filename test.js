f7e2a564ff689b65900fe67a391795eb477865cc
pzljp <201321060452@std.edu.cn>
2016-06-07 23:17:49 +0800

    test.js

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
 function drag(id)
-{  var obj;
-   var aaa;
-   var 2;//这是暂存区
-   var 3;//这是工作区
-   var 4;
+{  
    var obj = document.getElementById(id)
    obj.onmousedown = function(){
-        var 4;
-        var 5;
-        var 6;
-        var 7;
-        var 8;
    }
 }
